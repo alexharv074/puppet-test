bf9a6037c18f7b715a55fa51808722d0d17f2e18
Alex Harvey <Alex.Harvey@team.telstra.com>
2016-03-28 20:27:36 +1100

    Example showing how to generate a YAML catalog

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,2 @@
 class test {
-  $myval = alex('world')
-  notify { "alex() returned $myval": }
-
-  $myusers = {
-    'nick' => { uid => '1330' },
-    'dan'  => { uid => '1308' },
-  }
-  $defaults = {
-    'ensure' => present,
-  }
-  create_resources(user, $myusers, $defaults)
 }
