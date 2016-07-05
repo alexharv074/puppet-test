87728b4c8fc8c01caa673f4472413c3f205888c2
Alex Harvey <alexharv074@gmail.com>
2016-07-05 22:45:34 +1000

    https://ask.puppet.com/question/27012/setlookup-mysql-ruby-array-puppet-v4/

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,55 +1,14 @@
 class test {
- $home_dir = '/tmp'
- $install_dir = 'app'
- $amq_user = 'amq'
- $amq_group = 'amq'
- 
- file { "$home_dir/$install_dir/conf": ensure => directory }
- file { "$home_dir/$install_dir/bin": ensure => directory }
- file { "$home_dir/$install_dir/bin/activemq":
-   content => '
-#!/bin/bash
-if [ $1 = start ]; then
-  echo running > /tmp/status
-elif [ $1 = stop ]; then
-  echo stopped > /tmp/status
-fi
-exit 0
-',
+  $mysql = {
+      "jdbc_nodes_read" => [
+         "mysql101.x.com"
+      ],
+      "jdbc_nodes_write" => [
+         "mysql-clusterdb101.x.com",
+         "mysql-clusterdb102.x.com",
+         "mysql-clusterdb103.x.com"
+      ],
   }
-
- file { "$home_dir/$install_dir/conf/activemq.xml":
-   ensure       => present,
-   owner        => $amq_user,
-   group        => $amq_group,
-   content      => template('test/activemq.xml.erb'),
-   audit        => content,
-   notify       => Exec['serviceStop'],
- }
-
- file { "$home_dir/$install_dir/conf/jetty.xml":
-   ensure  => present,
-   owner   => $amq_user,
-   group   => $amq_group,
-   content => template('test/jetty.xml.erb'),
-   audit   => content,
-   notify  => Exec['serviceStop'],
- }
-
- exec {'serviceStop':
-   command => "$home_dir/$install_dir/bin/activemq stop",
-   user    => $amq_user,
-   group   => $amq_group,
-   #refresh => true,
-   notify  => Exec['serviceStart'],
-   onlyif  => '/bin/cat /tmp/status | grep -v grep | grep running 2>/dev/null',
- }
-
- exec {'serviceStart':
-   command     => "$home_dir/$install_dir/bin/activemq start",
-   user        => $amq_user,
-   group       => $amq_group,
-   refreshonly => true,
-   unless      => '/bin/cat /tmp/status | grep -v grep | grep running 2>/dev/null',
- }
+  $content = template('test/mytemplate.erb')
+  notify { $content: }
 }
--- spec/classes/test_spec.rb
+++ spec/classes/test_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe 'test' do
-  it { is_expected.to compile.with_all_deps }
+  it { File.write('myclass.json', PSON.pretty_generate(catalogue)) }
 end
