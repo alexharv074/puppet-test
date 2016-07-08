654d478d0bdc909af9aceeba8a3aa75e1afb4fc0
Alex Harvey <alexharv074@gmail.com>
2016-07-09 01:23:05 +1000

    Trying alias lookup in hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,4 @@
 class test {
-  $mysql = {
-      "jdbc_nodes_read" => [
-         "mysql101.x.com"
-      ],
-      "jdbc_nodes_write" => [
-         "mysql-clusterdb101.x.com",
-         "mysql-clusterdb102.x.com",
-         "mysql-clusterdb103.x.com"
-      ],
-  }
-  $content = template('test/mytemplate.erb')
-  notify { $content: }
+  $myarray = hiera('myarray')
+  notify { "${myarray[0]}": }
 }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,5 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
-  c.default_facts = {
-    :kernel                    => 'Linux',
-    :osfamily                  => 'RedHat',
-    :operatingsystem           => 'RedHat',
-    :operatingsystemrelease    => '6.5',
-    :operatingsystemmajrelease => '6',
-  }
+  c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
 end
