74bbca718866613021c610da6482ca3cf477bbc2
Alex Harvey <alexharv074@gmail.com>
2016-09-22 21:20:25 +1000

    whatever

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,6 @@
-class test {
+class test (
+  $foo = $test::params::foo,
+) inherits test::params {
 
   file { '/etc/httpd/conf.d/example.com.conf':
     content => template('test/mytemplate.erb'),
