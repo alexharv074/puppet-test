f6050757686e28d02c6e153ee96ce84eb8db545b
Alex Harvey <alexharv074@gmail.com>
2017-05-24 02:11:55 +1000

    first go

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,45 +1,31 @@
-class foo (
-) {
-  $mountpoints = {
-    '/' => {
-      available => '21.93 GiB',
-      available_bytes => '23542669312',
-      capacity => '26.88%',
-      device => '/dev/sda1',
-      filesystem => 'xfs',
-      options => ['rw', 'seclabel', 'relatime', 'attr2', 'inode64', 'noquota'],
-      size => '29.98 GiB',
-      size_bytes => '32195481600',
-      used => '8.06 GiB',
-      used_bytes => '8652812288',
-    },
-    '/datadisks/disk2' => {
-      available => '1006.74 GiB',
-      available_bytes => '1080982728704',
-      capacity => '0.01%',
-      device => '/dev/sdc1',
-      filesystem => 'ext4',
-      options => ['rw', 'seclabel', 'nosuid', 'nodev', 'noexec', 'noatime', 'nodiratime', 'data=ordered'],
-      size => '1006.82 GiB',
-      size_bytes => '1081062445056',
-      used => '76.02 MiB',
-      used_bytes => '79716352',
-    },
-    '/mnt/resource' => {
-      available => '110.06 GiB',
-      available_bytes => '118173499392',
-      capacity => '0.05%',
-      device => '/dev/sdb1',
-      filesystem => 'ext4',
-      options => ['rw', 'seclabel', 'relatime', 'data=ordered'],
-      size => '110.12 GiB',
-      size_bytes => '118236442624',
-      used => '60.03 MiB',
-      used_bytes => '62943232',
-    },
+class foo () {
+  define my_vcs_repo ($myRepo, $myTag) {
+    vcsrepo { "$myRepo-$myTag":
+      path     => "/home/user/$myRepo-$myTag",
+      source   => "git@github.com:7yl4r/$myRepo.git",
+      revision => 'production',  # aka branch
+      ensure   => latest,
+      provider => git,
+    }
   }
-  $keys = $mountpoints.keys.filter |$items| { $items =~ /datadisks/ }
-  notice($keys)
-  $datadisks = $mountpoints.filter |$items| { $items[0] =~ /datadisks/ }
-  notice($datadisks)
+
+  $data = [
+    {
+      myRepo => testRepo,
+      myTag  => testTag1,
+    },
+    {
+      myRepo => testRepo,
+      myTag  => testTag2,
+    },
+  ]
+
+  $data.each |$i, $ref| {
+    $myRepo = $ref['myRepo']
+    $myTag  = $ref['myTag']
+    foo::my_vcs_repo { "$myRepo-$i":
+      myRepo => $myRepo,
+      myTag => $myTag,
+    }
+  }
 }
