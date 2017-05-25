f21474fc0774d8f3ff824ee6fd22cb0b37c24223
Alex Harvey <alexharv074@gmail.com>
2017-05-25 20:58:13 +1000

    second go https://stackoverflow.com/questions/44118429/how-to-work-around-vcsrepo-duplicate-declaration-evaluation-error/44133546

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,31 +1,14 @@
-class foo () {
-  define my_vcs_repo ($myRepo, $myTag) {
-    vcsrepo { "$myRepo-$myTag":
-      path     => "/home/user/$myRepo-$myTag",
-      source   => "git@github.com:7yl4r/$myRepo.git",
-      revision => 'production',  # aka branch
-      ensure   => latest,
-      provider => git,
-    }
+class foo(
+  $my_repo = undef,
+  $my_tag  = undef,
+){
+  @vcsrepo { $my_repo:
+    path     => "/home/user/$my_repo",
+    source   => 'git@github.com:7yl4r/$my_repo.git',
+    ensure   => latest,
+    provider => git,
   }
+  realize Vcsrepo[$my_repo]
 
-  $data = [
-    {
-      myRepo => testRepo,
-      myTag  => testTag1,
-    },
-    {
-      myRepo => testRepo,
-      myTag  => testTag2,
-    },
-  ]
-
-  $data.each |$i, $ref| {
-    $myRepo = $ref['myRepo']
-    $myTag  = $ref['myTag']
-    foo::my_vcs_repo { "$myRepo-$i":
-      myRepo => $myRepo,
-      myTag => $myTag,
-    }
-  }
+  # then declare resources specific to my_tag
 }
