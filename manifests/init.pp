10d9bc4e8dbb2c9e8b05b29c61d9d582f64c27b6
Alex Harvey <alexharv074@gmail.com>
2017-06-11 15:03:48 +1000

    https://stackoverflow.com/questions/44474945/erb-iterate-over-array-of-hashes/44475556

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,7 @@
 class foo(
-  $my_repo = undef,
-  $my_tag  = undef,
 ){
-  @vcsrepo { $my_repo:
-    path     => "/home/user/$my_repo",
-    source   => 'git@github.com:7yl4r/$my_repo.git',
-    ensure   => latest,
-    provider => git,
+  $myservers = hiera('myservers')
+  file { '/tmp/foo': 
+    content => template('foo/mytemplate.erb')
   }
-  realize Vcsrepo[$my_repo]
-
-  # then declare resources specific to my_tag
 }
