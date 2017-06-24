ea72b154260d7d054f91e3cb70c9bc1e94cf762f
Alex Harvey <alexharv074@gmail.com>
2017-06-24 23:49:30 +1000

    https://stackoverflow.com/questions/44735263/rspec-puppet-how-to-test-for-a-define-as-a-member-of-a-class/44736978#44736978

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,31 +1,12 @@
 class foo {
-  $connections = {
-    'connection_name1' => {
-      'username' => 'user1',
-      'password' => 'pass1',
-    },
-    'connection_name2' => {
-      'username' => 'user2',
-      'password' => 'pass2',
-    },
-  }
-
-  $jdbc_default = {
-    'testWhileIdle'                => true,
-    'testOnBorrow'                 => true,
-    'testOnReturn'                 => false,
-    'timeBetweenEvictionRunsMillis'=> '30000',
-    'maxActive'                    => '20',
-    'maxWait'                      => '10000',
-    'initialSize'                  => '5',
-    'removeAbandonedTimeout'       => '600',
-    'removeAbandoned'              => false,
-    'logAbandoned'                 => true,
-    'minEvictableIdleTimeMillis'   => '30001',
-  }
-
-  file { '/tmp/myfile':
-    ensure   => file,
-    content => template('foo/mytemplate.erb'),
+  case $::operatingsystem {
+    'Darwin': {
+      file { '/tmp/foo':
+        ensure => file,
+      }
+    }
+    'RedHat': {
+    }
+    default: { warning("OS : ${::operatingsystem} is not (yet) supported") }
   }
 }
--- spec/classes/test_spec.rb
+++ spec/classes/test_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
-describe 'foo' do
-  it { File.write('myclass.json', PSON.pretty_generate(catalogue)) }
+describe 'foo', :type => :class do
+  context 'Darwin' do
+    let(:facts) {{:operatingsystem => 'Darwin'}}
+    it {
+      is_expected.to contain_class('foo')
+    }
+    it {
+      is_expected.to contain_file('/tmp/foo').with(
+        {
+          :ensure => 'file',
+        }
+      )
+    }
+    it { File.write('myclass.json', PSON.pretty_generate(catalogue)) }
+  end
+
+  context 'foo' do
+    let(:facts) {{:operatingsystem => 'foo'}}
+    it { File.write('myclass.json', PSON.pretty_generate(catalogue)) }
+  end
 end
