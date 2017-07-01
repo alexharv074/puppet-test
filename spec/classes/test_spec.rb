bd7db5181893da0e34289e82baabfd2f61205b93
Alex Harvey <alexharv074@gmail.com>
2017-07-02 00:19:10 +1000

    https://stackoverflow.com/questions/44827714/getting-archive-tmp-filename-doesnt-seem-to-be-in-the-catalog-while-exec

--- spec/classes/test_spec.rb
+++ spec/classes/test_spec.rb
@@ -1,23 +1,5 @@
 require 'spec_helper'
 
 describe 'foo', :type => :class do
-  context 'Darwin' do
-    let(:facts) {{:operatingsystem => 'Darwin'}}
-    it {
-      is_expected.to contain_class('foo')
-    }
-    it {
-      is_expected.to contain_file('/tmp/foo').with(
-        {
-          :ensure => 'file',
-        }
-      )
-    }
-    it { File.write('myclass.json', PSON.pretty_generate(catalogue)) }
-  end
-
-  context 'foo' do
-    let(:facts) {{:operatingsystem => 'foo'}}
-    it { File.write('myclass.json', PSON.pretty_generate(catalogue)) }
-  end
+  it { File.write('myclass.json', PSON.pretty_generate(catalogue)) }
 end
