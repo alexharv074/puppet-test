6055c9ea1408da8412082a7a827e3d639dea6821
Alex Harvey <alexharv074@gmail.com>
2017-07-14 00:21:56 +1000

    stackoverflow.com/questions/45046191/puppet-with-multiple-nfs-mount-on-same-server/45050317

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,63 +1,63 @@
 class foo {
-  $sourcefile = $osfamily ? {
-    'Solaris' => 'puppet:///modules/foo/splunkforwarder-6.5.2-67571ef4b87d-SunOS-sparc.tar.Z',
-    'RedHat' => 'puppet:///modules/foo/splunkforwarder-6.5.2-67571ef4b87d-Linux-x86_64.tgz',
-    'AIX' => 'puppet:///modules/foo/splunkforwarder-6.5.2-67571ef4b87d-AIX-powerpc.tgz',
+  # Hiera lookups
+  $location = 'x.x.x.x:/stats'
+  $location2 = 'x.x.x.x:/scratch'
+
+  tag        'new_mount'
+
+  file { '/data': ensure => directory }
+
+  file { '/data/stats':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => File['/data'],
+    tag     => 'new_mount',
   }
 
-  $dirname = 'splunkforwarder'
-  $filename = "${dirname}.tar.gz"
-  $install_path = "/opt/${dirname}"
-
-  file { $install_path:
-    ensure => directory,
-    owner  => 'findev',
-    group  => 'finacle',
-    mode   => '0777',
+  mount { '/data/stats':
+    ensure  => mounted,
+    fstype  => 'nfs',
+    device  => $location,
+    options => 'defaults,nodev,nosharecache',
+    require => File['/data/stats'],
+    tag     => 'new_mount'
   }
 
-  archive { $filename:
-    path         => "/tmp/${filename}",
-    source       => $sourcefile,
-    extract      => true,
-    extract_path => '/opt',
-    creates      => "${install_path}",
-    cleanup      => true,
-    user         => 'findev',
-    group        => 'finacle',
-    require      => File[$install_path],
+  file { '/data/ops':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'mail',
+    mode    => '0775',
+    require => File['/data'],
+    tag     => 'new_mount',
   }
 
-  exec {'start_splunk_service':
-    path        => '/usr/bin:/bin:/usr/sbin:/sbin',
-    user        => 'findev',
-    command     => '/opt/splunkforwarder/bin/splunk start --accept-license && echo "1" > /tmp/splunk_status.txt',
-    onlyif      => [ 
-      "test -f /opt/splunkforwarder/bin/splunk",
-      "test `ps -ef | grep -v grep | grep splunk | wc -l` -eq 0",
-    ],
-    environment => ["HOME=/app/finacle"],
-    require     => Archive[$filename],
+  mount { '/data/ops':
+    ensure  => mounted,
+    fstype  => 'nfs',
+    device  => $location2,
+    options => 'defaults,nodev,nosharecache',
+    require => File['/data/ops'],
+    tag     => 'new_mount',
   }
 
-  file { '/opt/splunkforwarder/etc/system/local/deploymentclient.conf': 
-    ensure  => present,
-    owner   => 'findev',
-    group   => 'finacle',
-    mode    => "0777",
-    source  => 'puppet:///modules/aoa_agent_installation/deploymentclient.conf',
-    require => Archive[$filename],
+  file { '/data/scratch':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'mail',
+    mode    => '0775',
+    require => File['/data'],
+    tag     => 'new_mount',
   }
 
-  exec {'restart_start_splunk_service':
-    path        => '/usr/bin:/bin:/usr/sbin:/sbin',
-    user        => 'findev',
-    command     => '/opt/splunkforwarder/bin/splunk restart && echo "0" > /tmp/splunk_status.txt',
-    onlyif      => [ 
-      "test -f /opt/splunkforwarder/bin/splunk",
-      "test `cat /tmp/splunk_status.txt | tr -s ' '` -eq 1",
-    ],
-    environment => ["HOME=/app/finacle"],
-    require     => File['/opt/splunkforwarder/etc/system/local/deploymentclient.conf'],
+  mount { '/data/scratch':
+    ensure  => mounted,
+    fstype  => 'nfs',
+    device  => $location2,
+    options => 'defaults,nodev,nosharecache',
+    require => File['/data/scratch'],
+    tag     => 'new_mount',
   }
 }
