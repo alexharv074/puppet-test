ea303e64de26b901d302d63e187026a85ac553a0
Alex Harvey <alexharv074@gmail.com>
2017-07-29 22:10:11 +1000

    https://stackoverflow.com/questions/45387586/using-puppet-hash-for-epp-templates/45389071
    ERB v EPP example.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,24 @@
-class foo (
-  $servers,
-  $nameserver,
-) {
-  notify { "servers: $servers": }
-  notify { "nameserver: $nameserver": }
+class foo () {
+  $proxy_cache_path = {
+    'apples'  => 1,
+    'bananas' => 2,
+  }
+  $proxy_cache_levels = 2
+  $proxy_cache_max_size = 2
+  $proxy_cache_inactive = 2
+
+  file { '/foo':
+    ensure  => file,
+    content => template('foo/mytemplate.erb')
+  }
+
+  file { '/bar':
+    ensure  => file,
+    content => epp('foo/mytemplate.epp', {
+      'proxy_cache_path'     => $proxy_cache_path,
+      'proxy_cache_levels'   => $proxy_cache_levels,
+      'proxy_cache_max_size' => $proxy_cache_max_size,
+      'proxy_cache_inactive' => $proxy_cache_inactive,
+    }),
+  }
 }
--- spec/classes/test_spec.rb
+++ spec/classes/test_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe 'foo', :type => :class do
+  it "content in foo should be the same as in bar" do
+    foo = catalogue.resource('file', '/foo').send(:parameters)[:content]
+    bar = catalogue.resource('file', '/bar').send(:parameters)[:content]
+    expect(foo).to eq bar
+  end
+
   it { File.write('myclass.json', PSON.pretty_generate(catalogue)) }
 end
