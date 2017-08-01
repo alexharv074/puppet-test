85ddf7754a990587db19ff43bc6dbc884af0c9b9
Alex Harvey <alexharv074@gmail.com>
2017-08-01 18:13:43 +1000

    https://stackoverflow.com/questions/44270544/converting-puppet-erb-template-to-epp?

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,24 +1,17 @@
 class foo () {
-  $proxy_cache_path = {
-    'apples'  => 1,
-    'bananas' => 2,
+  $filter = {
+    'filtertype' => 'foo',
+    'apples'     => 1,
+    'bananas'    => 2,
   }
-  $proxy_cache_levels = 2
-  $proxy_cache_max_size = 2
-  $proxy_cache_inactive = 2
 
   file { '/foo':
     ensure  => file,
-    content => template('foo/mytemplate.erb')
+    content => template('foo/mytemplate.erb'),
   }
 
   file { '/bar':
     ensure  => file,
-    content => epp('foo/mytemplate.epp', {
-      'proxy_cache_path'     => $proxy_cache_path,
-      'proxy_cache_levels'   => $proxy_cache_levels,
-      'proxy_cache_max_size' => $proxy_cache_max_size,
-      'proxy_cache_inactive' => $proxy_cache_inactive,
-    }),
+    content => epp('foo/mytemplate.epp', {'filter' => $filter}),
   }
 }
