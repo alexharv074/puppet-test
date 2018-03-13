3e80a92f034909a10387d66c414c5e9f6c634d1f
Alex Harvey <alex@razorconsulting.com.au>
2018-03-13 10:33:49 +0000

    Avoid call to shell

--- rob.rb
+++ rob.rb
@@ -3,11 +3,10 @@
 require 'inifile'
 require 'pp'
 
-apps = `/usr/bin/find /private/tmp -name app.conf|/usr/bin/awk -F/ '{print $(NF-2)}'|sort -r| uniq`
-
 attributes = {}
 
-apps.split("\n").each do |app|
+apps = Dir.glob("/tmp/**/app.conf").map { |f| f.split('/')[2] }
+apps.each do |app|
   myini = IniFile.load("/tmp/#{app}/default/app.conf")
   myini.each_section do |section|
     attributes[app] = myini[section]
