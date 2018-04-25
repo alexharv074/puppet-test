e859ec8b6726dadf21c286c9da4310542739ebe6
Alex Harvey <alex@razorconsulting.com.au>
2018-04-25 18:16:35 +1000

    https://stackoverflow.com/questions/50016045/test-for-undefined-fact-in-puppet/50016390

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,7 @@
 class test {
-  exec { '/bin/mkdir -p /tmp/bar ; /usr/bin/touch /tmp/bar/{a,b,c,d,f}': }
-
-  file { '/tmp/foo':
-    ensure  => directory,
-    source  => '/tmp/bar',
-    recurse => true,
-    purge   => true,
-  }
-
-  file { '/tmp/foo/e':
-    ensure => file,
+  include stdlib
+  if has_key($facts['processors'], 'count') {
+    notice("All OK")
   }
 }
 
