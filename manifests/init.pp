13973aeff4b136ff9e7473ef26e1b4a0f4fd3ecb
Alex Harvey <alex@razorconsulting.com.au>
2018-06-09 21:02:38 +1000

    proof of concept
    Using file_line to allow Puppet to manage security in a Jenkins
    config.xml file, while allowing a manual process to also update the
    file.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,25 @@
 class test {
-file { '/tmp/dest':
-  ensure             => directory,
-  links              => follow,
-  source             => '/tmp/symlink',
-  recurse            => true,
-  source_permissions => ignore,
-  force              => true,
-}
+
+  $users = ['bill','ted']
+
+  file_line { 'use_security':
+    ensure => present,
+    path   => '/tmp/config.xml',
+    line   => '  <useSecurity>true</useSecurity>',
+    match  => "^  *<useSecurity>.*</useSecurity>",
+  }
+
+  # We need this content all on one line to workaround file_line bug
+  # See https://tickets.puppetlabs.com/browse/MODULES-4006.
+  #
+  $content = inline_template('  <authorizationStrategy class="hudson.security.GlobalMatrixAuthorizationStrategy"><%- @users.sort.each do |username| -%><permission>hudson.model.Hudson.Administer:<%= username %></permission><%- end -%></authorizationStrategy>')
+
+  file_line { 'authorization_strategy':
+    ensure => present,
+    path   => '/tmp/config.xml',
+    line   => $content,
+    after  => '</useSecurity>$'
+  }
 }
 
 include test
