27d25bf9b4aff361db877c840382080c7964829d
Alex Harvey <alexharv074@gmail.com>
2018-08-24 17:45:31 +1000

    https://stackoverflow.com/questions/51998644/pick-a-nested-hash-which-has-a-given-key-value-pair-in-puppet/51999687#51999687

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,35 @@
-class test {
-
-  $val = 'foo-bar.tct'
-
-  $server = ['123.123.1.1','123.123.345.33','123.123.345.33']
-  $restrict = ['1.1.1.1','255.255.255.224']
-
-  file { '/tmp/bar':
-    ensure  => file,
-    content => template("test/mytemplate.erb"),
+class test () {
+  $server = {
+    'host' => 'foo.bar.baz',
+    'public_host' => {
+      'common' => {
+        'host' => 'foo.bar.baz',
+      },
+      'internal' => {
+        'host' => 'some.internal.name',
+        'partition' => {
+          'name' => 'int',
+          'service_port' => {8443=>nil}
+        }
+      },
+      'nonvs' => {
+        'partition' => {
+          'name' => 'BLA',
+          'manage_virtualserver' => false,
+        }
+      }
+    }
   }
+
+  $public_host = $server['public_host']
+
+  $filtered_data = $public_host.filter |$k, $v| {
+    has_key($public_host[$k], 'partition') and
+    has_key($public_host[$k]['partition'], 'name') and
+      $public_host[$k]['partition']['name'] == 'BLA'
+  }
+
+  notice($filtered_data)
 }
 
 include test
--- spec/classes/test_spec.rb
+++ spec/classes/test_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe 'test' do
+  let(:params) do
+    {
+      'ensure' => 'present',
+    }
+  end
+
   it 'should write a compiled catalog' do
     is_expected.to compile.with_all_deps
     File.write(
