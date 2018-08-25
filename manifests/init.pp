faf60b3f919a1ef8fa96478e0978310d795c7c1e
Alex Harvey <alexharv074@gmail.com>
2018-08-25 20:27:53 +1000

    More on that

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,18 +17,43 @@ class test () {
           'name' => 'BLA',
           'manage_virtualserver' => false,
         }
-      }
+      },
+      'foovs' => {
+        'partition' => {
+          'name' => 'BLA',
+          'manage_virtualserver' => true,
+        }
+      },
     }
   }
 
   $public_host = $server['public_host']
 
-  $filtered_data = $public_host.filter |$k, $v| {
-    has_key($v, 'partition') and has_key($v['partition'], 'name')
-      and $v['partition']['name'] == 'BLA'
+  $filtered_data = $public_host.filter |$x| {
+    has_key($x[1], 'partition') and has_key($x[1]['partition'], 'name') and $x[1]['partition']['name'] == 'BLA'
+  }
+  
+  notice($filtered_data)
+
+  $filtered = {
+    'a' => {
+      'd' => 1,
+    },
+    'b' => {
+      'e' => 2,
+    },
+    'c' => {
+      'f' => 3,
+    },
   }
 
-  notice($filtered_data)
+  notice($filtered)
+
+  $reduced_data = $filtered.reduce({}) |$memo, $x| {
+    $memo + $x[1]
+  }
+
+  notice($reduced_data)
 }
 
 include test
