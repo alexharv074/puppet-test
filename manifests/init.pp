96689e698e6071e01f51f1d03cde498bbdc30db5
Alex Harvey <alexharv074@gmail.com>
2018-11-22 14:01:45 +1100

    Corrected yet again

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,15 @@
 class test {
   $clusters = hiera('elasticsearch::clusters')
-  $masterlist = $clusters.reduce([]) |$memo, $x| {
-    $memo + $x[1]['masterlist']
+  $myclusters = $clusters.filter |$cluster, $data| {
+    ($::fqdn in $data['masterlist']) or ($::fqdn in $data['kibanalist'])
   }
-  if ! ($::fqdn in $masterlist) {
+  if ($myclusters.empty) {
     notify { 'No cluster for node':
       message => "${::fqdn} is not configured to be in any cluster in the hiera data",
     }
   }
+  $cluster_name = $myclusters.keys[0]
+  notice($cluster_name)
 }
 
 include test
