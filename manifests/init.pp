74105693941b7e14ef97bce69f08d956e9fac0ce
Alex Harvey <alexharv074@gmail.com>
2018-12-06 14:32:16 +1100

    Cleaner

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,12 @@
 class test {
   $servers_list = hiera('my_list::servers')
-  $filtered = $servers_list.filter |$key, $value| { $key != $::fqdn }
-  $mapped   = $filtered.map |$key, $value| { "broker-${value['id']}-check" }
-  $joined   = join($mapped, ',')
-  notify {"******* ${joined}": }
+  $list_broker = join(
+    $servers_list
+      .filter |$key, $value| { $key != $::fqdn }
+      .map    |$key, $value| { "broker-${value['id']}-check" },
+    ','
+  )
+  notify {"******* ${list_broker}": }
 }
 
 include test
