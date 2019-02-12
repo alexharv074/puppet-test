094aecc413ed52c581375e83162a97a62de769eb
Alex Harvey <alexharv074@gmail.com>
2019-02-12 20:32:20 +1100

    https://stackoverflow.com/q/54646191/3787051

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,21 @@
 class test () {
-  $dovecot_version = '2.1'
-  notice(template('test/mytemplate.erb'))
+  file { 'folder_a':
+    source  => 'puppet:///modules/folder_a',
+    ensure  => 'directory',
+    recurse => true,
+    path    => 'C:/folder_a',
+    source_permissions => ignore,
+    notify  => Exec['install.bat'],
+  }
+
+  exec { 'test_cmd':
+    path      => $::path,
+    command   => 'cmd.exe /c C:/test.cmd',
+    provider  => windows,
+    subscribe => File['folder_a'],
+    logoutput => true,
+    refreshonly => true,
+  }
 }
 
 include test
