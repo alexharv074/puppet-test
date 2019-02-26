d7661650231bd1b487d57601926bb92e6569bd5a
Alex Harvey <alexharv074@gmail.com>
2019-02-26 13:40:19 +1100

    https://stackoverflow.com/q/54871219/3787051
    Puppet 6 account_users and sshkeys_core in .fixtures.yml with
    create_resources.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,7 @@
-class test () {
-  file { 'folder_a':
-    source  => 'file:///tmp/source',
-    source_permissions => ignore,
-    ensure  => 'directory',
-    recurse => true,
-    path    => '/tmp/folder_a',
-  }
-
-  exec { 'test_cmd':
-    command     => '/bin/echo hello',
-    subscribe   => File['folder_a'],
-    refreshonly => true,
-    logoutput   => true,
-  }
+class test (
+  Hash $users,
+) {
+  create_resources(accounts::user, $users)
 }
 
-include test
+#include test
