e2afd31ffb34ebbc59c7665dc5234b1966e144f5
Alex Harvey <alexharv074@gmail.com>
2019-03-13 13:17:49 +1100

    Example showing parseyaml, and file function

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,8 @@
-class test (
-  Hash $users,
-) {
-  create_resources(accounts::user, $users)
+class test {
+  $myyaml = parseyaml(file('test/myyaml.yaml'))
+  notice($myyaml)
+  $myjson = parsejson(file('test/myjson.json'))
+  notice($myjson)
 }
 
-#include test
+include test
