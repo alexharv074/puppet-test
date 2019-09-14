dadc6e7493de449b6cfdf2d8b0259302077c0c5f
Alex Harvey <alexharv074@gmail.com>
2019-09-14 21:10:41 +1000

    https://stackoverflow.com/a/57934865/3787051

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,12 @@
-class test (
-	exec { 'certbot_nginx' :
-		command => 'for file in $(find /tmp/alex -type f); do if [ "$(grep "renew_hook = service nginx reload" $file)" != "renew_hook = service nginx reload" ]; then sed -i 's/\(\[renewalparams\]\)/\1\nrenew_hook = service nginx reload/' $file; fi; done',
-    onlyif  => 'systemctl -q is-active certbot.timer && systemctl -q is-active nginx',
-	}
-
-  exec { "bash -c "if [ \"$(systemctl -q is-active certbot.timer && echo $?)\" = \"0\" ] && [ \"$(systemctl -q is-active nginx && echo $?)\" = \"0\" ]; then for file in $(find /etc/letsencrypt/renewal -type f); do if [ \"$(grep \"renew_hook = service nginx reload\" $file)\" != \"renew_hook = service nginx reload\" ]; then sed -i 's/\(\[renewalparams\]\)/\1\nrenew_hook = service nginx reload/' $file; fi; done; fi"" :
-    provider => shell,
-    command => 'bash -c "if [ \"$(systemctl -q is-active certbot.timer && echo $?)\" = \"0\" ] && [ \"$(systemctl -q is-active nginx && echo $?)\" = \"0\" ]; then for file in $(find /etc/letsencrypt/renewal -type f); do if [ \"$(grep \"renew_hook = service nginx reload\" $file)\" != \"renew_hook = service nginx reload\" ]; then sed -i 's/\(\[renewalparams\]\)/\1\nrenew_hook = service nginx reload/' $file; fi; done; fi"',
+class test {
+  file_line { 'qux':
+    path => '/tmp/alex',
+    line => 'qux',
+  }
+  exec { '/bin/echo hello world':
+    refreshonly => true,
+    subscribe   => File_line['qux'],
   }
 }
+
+include test
