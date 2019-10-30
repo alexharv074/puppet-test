c7a0a149e92ea6142a6ec00c9baa06ea6f933442
Alex Harvey <alexharv074@gmail.com>
2019-10-30 20:51:28 +1100

    https://stackoverflow.com/a/58621465/3787051

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,26 @@
-class test {
-  wait_for { '/usr/bin/false':
-    exit_code         => 2,
-    polling_frequency => 0.3,
-    max_retries       => 5,
+$content = 'xxx'
+
+class klass1 {
+  file { '/tmp/abc':
+    content => $::content,
   }
 }
 
-include test
+# Stage 0 creates the file /tmp/abc.
+class stage0 {
+  contain klass1
+}
+
+# Stage 1 uses the contents of /tmp/abc to create the
+# file /tmp/def.
+class stage1 {
+  file { '/tmp/def':
+    content => "Contents: ${::content}",
+  }
+}
+
+# Try to force stage0 to be loaded before stage1.
+include stage0
+class { 'stage1':
+  require => Class['stage0']
+}
