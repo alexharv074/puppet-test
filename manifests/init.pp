c9d657d4ab673992c1358c8198a635e22522fa06
Alex Harvey <alexharv074@gmail.com>
2019-11-17 01:46:04 +1100

    Forgotten

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,3 @@
-class klass1 {
-  file { '/tmp/abc':
-    content => 'xxx',
-  }
-}
-
-# Stage 0 creates the file /tmp/abc.
-class stage0 {
-  contain klass1
-}
-
-# Stage 1 uses the contents of /tmp/abc to create the
-# file /tmp/def.
-class stage1 {
-  file { '/tmp/def':
-    content => inline_template("<% contents = begin
-                File.read('/tmp/abc')
-              rescue
-                ''
-              end -%>
-Contents: <%= contents %>")
-  }
-}
-
-# Try to force stage0 to be loaded before stage1.
-include stage0
-class { 'stage1':
-  require => Class['stage0']
-}
+$h = downcase(regsubst($facts['hostname'], '..(.)$', '01\1'))
+$n = "${h}.ex-ample.com"
+notice($n)
