03d9e9e265d53d01b3c1cac8dc8156799511ba43
Alex Harvey <alexharv074@gmail.com>
2019-12-01 00:47:49 +1100

    https://stackoverflow.com/q/59106074/3787051

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,35 +1,7 @@
-class am_rgw_profile::am_rgw_ports {
-  if ('xarsiiq1xd' in $hostname) {
-    [
-      {
-        'port'     => '2470',
-        'protocol' => 'tcp',
-      },
-      {
-        'port'     => '2472',
-        'protocol' => 'tcp',
-      },
-    ]
-  } elsif ('xarsiiq1xe' in $hostname) {
-    [
-      {
-        'port'     => '2492',
-        'protocol' => 'tcp',
-      },
-      {
-        'port'     => '2516',
-        'protocol' => 'tcp',
-      },
-    ]
-  }
-  firewalld::custom_service{'am_rgw_ports':
-    short       => 'am_rgw_ports',
-    description => 'IIQ IaaS gateway wg6561',
-    port        => $port,
-  }
-  firewalld_service { 'Allow am_racf_ports services for IIQ gateway servers':
-    ensure  => 'present',
-    service => 'am_rgw_ports',
-    zone    => 'Cali'
-  }
+class test {
+  notice(inline_template("<% @example = scope().call_function('hiera',['profile::example::firewalls']) -%>
+show me the array!
+<%= @example %>"))
 }
+
+include test
